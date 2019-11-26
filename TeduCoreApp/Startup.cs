337566eae142902f20380dfa654e43ce53bd4678
KND <ky.nguyendinh@gmail.com>
2019-11-26 09:55:21 +0700

    Create Admin Area

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -103,6 +103,10 @@ namespace TeduCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
